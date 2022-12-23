ba5c654b015d4eec8cc449e4e8a006f1b3006611
kononiaka <kononiaka.vladimir@gmail.com>
2022-12-23 18:04:15 +0100

    comments logic was added

--- react-router-practice/src/components/comments/Comments.js
+++ react-router-practice/src/components/comments/Comments.js
@@ -1,15 +1,49 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
+import { useParams } from 'react-router-dom';
 
 import classes from './Comments.module.css';
 import NewCommentForm from './NewCommentForm';
+import useHttp from './../../hooks/hooks/use-http';
+import { getAllComments } from '../../lib/lib/api';
+import { useEffect } from 'react';
+import LoadingSpinner from './../UI/LoadingSpinner';
+import CommentsList from './CommentsList';
 
 const Comments = () => {
   const [isAddingComment, setIsAddingComment] = useState(false);
+  const params = useParams();
+
+  const { sendRequest, status, data: loadedComments } = useHttp(getAllComments);
+
+  const { quoteId } = params;
+
 
   const startAddCommentHandler = () => {
     setIsAddingComment(true);
   };
-  
+
+  const onAddCommentHandler = useCallback(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId, onAddCommentHandler]);
+
+  let comments;
+  if (status === 'pending') {
+    comments = <div className='centered'><LoadingSpinner></LoadingSpinner></div>;
+  }
+
+  if (status === 'completed' && loadedComments) {
+    comments = <CommentsList comments={loadedComments}></CommentsList>;
+  }
+
+  if (status === 'completed' && (!loadedComments || loadedComments.length === 0)) {
+    comments = <p className='centered'>No commets...</p>;
+  }
+
+
   return (
     <section className={classes.comments}>
       <h2>User Comments</h2>
@@ -18,8 +52,8 @@ const Comments = () => {
           Add a Comment
         </button>
       )}
-      {isAddingComment && <NewCommentForm />}
-      <p>Comments...</p>
+      {isAddingComment && <NewCommentForm quoteId={quoteId} onAddedComment={onAddCommentHandler} />}
+      {comments}
     </section>
   );
 };
