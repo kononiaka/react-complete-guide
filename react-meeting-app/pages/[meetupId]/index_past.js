fb54526542dcd9edd81365c057d5df7fbe8ae35a
kononiaka <kononiaka.vladimir@gmail.com>
2023-01-21 18:34:52 +0100

    mongodb description fixed

--- react-meeting-app/pages/[meetupId]/index_past.js
+++ react-meeting-app/pages/[meetupId]/index_past.js
@@ -19,9 +19,6 @@ export const getStaticPaths = async () => {
     const meetupsCollection = db.collection("meetups");
 
     const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
-
-    client.close();
-
     return {
         fallback: false,
         paths: meetups.map((meetup) => ({ params: { meetupId: meetup._id.toString() } }))
@@ -30,6 +27,7 @@ export const getStaticPaths = async () => {
 
 export const getStaticProps = async (context) => {
     const meetupId = context.params.meetupId;
+    // console.log(meetupId);
 
     const client = await MongoClient.connect("mongodb+srv://CondorAwful:qun3N8pruHGiC!g@cluster0.ecefcy2.mongodb.net/meetups?retryWrites=true&w=majority");
 
