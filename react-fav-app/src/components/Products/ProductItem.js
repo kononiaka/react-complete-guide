65c2de4465183c6c7ab5467de141d4fa755a5d09
kononiaka <kononiaka.vladimir@gmail.com>
2023-02-16 19:35:52 +0100

    use custom store

--- react-fav-app/src/components/Products/ProductItem.js
+++ react-fav-app/src/components/Products/ProductItem.js
@@ -1,14 +1,14 @@
-import React, { useContext } from 'react';
-import { ProductsContext } from '../../context/products-context';
-
+import React from 'react';
+import { useStore } from '../../hooks-store/store';
 import Card from '../UI/Card';
+
 import './ProductItem.css';
 
 const ProductItem = props => {
-  const toggleFav = useContext(ProductsContext).togFav;
+  const dispatch = useStore()[1];
 
   const toggleFavHandler = () => {
-    toggleFav(props.id);
+    dispatch('TOGGLE_FAV', props.id);
   };
 
   return (
